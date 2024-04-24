db645bf2d402073a58152d76146542aa45e9775d
EduardoBran <eduardo.ads1814@gmail.com>
2024-04-24 18:50:52 -0300

    fist commit

---
+++ 02-Mini-Projeto_-_Construcao_Treinamento_Avaliacao_e_Selecao_de_Modelos_Para_Classificacao_em_R.R
@@ -0,0 +1,22 @@
+####  Big Data Real-Time Analytics com Python e Spark  ####
+
+# Configurando o diretório de trabalho
+setwd("~/Desktop/DataScience/CienciaDeDados/2.Big-Data-Real-Time-Analytics-com-Python-e-Spark/8.Machine_Learning_em_Linguagem_Python")
+getwd()
+
+
+
+## Importando Pacotes
+library(readxl)         # carregar arquivos
+library(dplyr)          # manipula dados
+library(tidyr)          # manipula dados (funcao pivot_longer)
+library(ggplot2)        # gera gráficos
+library(patchwork)      # unir gráficos
+library(corrplot)       # mapa de Correlação
+library(caret)          # pacote preProcess para normalização
+
+library(randomForest)
+
+
+
+
