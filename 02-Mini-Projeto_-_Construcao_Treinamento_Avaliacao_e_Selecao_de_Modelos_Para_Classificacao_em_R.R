a46f17827fb56cfa5b348c65e16583604c71b076
EduardoBran <eduardo.ads1814@gmail.com>
2024-04-25 19:47:12 -0300

    apresentando o contexto do projeto Identificacao Risco Doenca

--- 02-Mini-Projeto_-_Construcao_Treinamento_Avaliacao_e_Selecao_de_Modelos_Para_Classificacao_em_R.R
+++ 02-Mini-Projeto_-_Construcao_Treinamento_Avaliacao_e_Selecao_de_Modelos_Para_Classificacao_em_R.R
@@ -15,9 +15,46 @@ library(patchwork)      # unir gráficos
 library(corrplot)       # mapa de Correlação
 library(caret)          # pacote preProcess para normalização
 
-library(randomForest)
+library(randomForest)   # algoritmo de ML
 
 
 
+
+############################  Modelagem Preditiva para Identificação de Risco de Doença Hepática  ############################  
+
+## Etapas:
+  
+# - Construção, Treinamento, Avaliação e Seleção de Modelos para Classificação
+
+## Introdução:
+
+# - Vamos trabalhar agora em nosso primeiro Mini-Projeto de Machine Learning, cujo objetivo é fornecer um passo a passo completo do processo de construção,
+#   treinamento, avaliação e seleção de modelos para classificação. Este projeto será abordado de maneira integral, desde a definição do problema de negócio
+#   até as previsões com o modelo treinado.
+
+## Contexto:
+
+# - O número de pacientes com doença hepática tem aumentado continuamente devido a fatores como consumo excessivo de álcool, inalação de gases nocivos,
+#   ingestão de alimentos contaminados, e uso de drogas e anabolizantes. Em resposta a essa crescente preocupação de saúde pública, este mini-projeto visa 
+#   construir um modelo de Machine Learning capaz de prever se um paciente irá ou não desenvolver uma doença hepática com base em várias características 
+#   clínicas e demográficas. Este modelo pode ser extremamente útil para médicos, hospitais ou governos no planejamento de orçamentos de saúde e na criação 
+#   de políticas de prevenção eficazes.
+
+## Objetivo:
+
+# - O objetivo é prever uma classe (sim ou não), usaremos aprendizado supervisionado para classificação, criando diferentes versões do modelo com diferentes 
+#   algoritmos e passaremos por todo o processo de Machine Learning de ponta a ponta. Usaremos como fonte de dados o dataset disponível no link a seguir.
+
+## Dados:
+
+# Link -> https://archive.ics.uci.edu/ml/datasets/ILPD+(Indian+Liver+Patient+Dataset)
+
+# - Os dados para este projeto são provenientes do "Indian Liver Patient Dataset", disponível no link acima. Este conjunto de dados contém registros de
+#   pacientes hepáticos e não hepáticos coletados na Índia. A coluna "Dataset" atua como um rótulo de classe, dividindo os indivíduos em pacientes com doença
+#   hepática (1) ou sem a doença (2).
+
+
+
+#### Carregando os Dados
 
 
